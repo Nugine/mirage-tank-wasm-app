36723895398f19005bcd053301ccc27f007932af
Nugine <nugine@foxmail.com>
2019-05-25 15:44:16 +0800

    enable typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ const path = require('path');
 
 module.exports = {
   entry: "./bootstrap.js",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.wasm']
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
